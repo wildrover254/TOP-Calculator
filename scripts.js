6e6c06a2b9e28d9add6242a82be0f2324846c7ec
James Winchester <wildrover254@gmail.com>
2022-04-15 15:55:21 -0400

    Created operator functions

---
+++ scripts.js
@@ -0,0 +1,24 @@
+let inputA;
+let inputB;
+let operator;
+let display;
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+ return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate() {
+
+}
